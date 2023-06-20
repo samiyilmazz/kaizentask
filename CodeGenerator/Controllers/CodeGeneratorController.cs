d3b1ad0574690220002e6b8b8c2ad15d244e174f
sami.yilmaz <ce.samiyilmaz@gmail.com>
2023-06-20 22:00:49 +0300

    Finished CodeGenerator

---
+++ CodeGenerator/Controllers/CodeGeneratorController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeGenerator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CodeGeneratorController : ControllerBase
+    {
+        private readonly HashSet<string> GeneratedCodeSet = new HashSet<string>();
+
+        [HttpGet("random", Name = "GetRandomCode")]
+        public ActionResult<string> GetRandomCode()
+        {
+            string generatedCode;
+            do
+            {
+                generatedCode = GenerateCode();
+            } while (!IsUnique(generatedCode));
+
+            if (!IsUnique(generatedCode))
+            {
+                throw new Exception("Error while creating a new unique code.");
+            }
+
+            GeneratedCodeSet.Add(generatedCode);
+            return Ok(generatedCode);
+        }
+
+        private string GenerateCode()
+        {
+            const string allowedChars = "ACDEFGHKLMNPRTXYZ234579";
+            const int codeLength = 8;
+
+            Random random = new Random();
+            char[] codeChars = new char[codeLength];
+            for (int i = 0; i < codeLength; i++)
+            {
+                codeChars[i] = allowedChars[random.Next(0, allowedChars.Length)];
+            }
+            return new string(codeChars);
+        }
+
+        private bool IsUnique(string code)
+        {
+            return !GeneratedCodeSet.Contains(code);
+        }
+    }
+}
